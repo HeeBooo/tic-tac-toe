ce7898bc04dd48f51ef29b3f571b472382e84804
HeeBooo <1807941596@qq.com>
2017-10-07 15:32:33 +0800

    finished this game

--- src/index.js
+++ src/index.js
@@ -42,6 +42,11 @@ class Board extends Component {
         // 而深复制则是开辟新的栈，两个对象对应两个不同的地址，修改一个对象的属性，
         // 不会改变另一个对象的属性。深复制实现代码如下：可以从两个方法进行解决。
         const squares = this.state.squares.slice();
+
+        // 已经落子或有一方获胜
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         // 每走一步都需要切xIsNext的值来实现轮流
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
@@ -62,7 +67,14 @@ class Board extends Component {
     };
 
     render () {
-        const status ='Next player:' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner:' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div>
                 <div className="status">{status}</div>
@@ -101,6 +113,28 @@ class Game extends Component {
             
         )
     }
+}
+
+// 判断赢家
+function calculateWinner (squares) {
+    // 以下条件为赢
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 ReactDOM.render(
