1850c9ec46b87d10bccf90bcded170b56a9d0f74
HeeBooo <1807941596@qq.com>
2017-10-07 16:42:51 +0800

    1.0 finished

--- src/index.js
+++ src/index.js
@@ -24,60 +24,20 @@ function Square(props) {
 
 // 包含了九个格子的容器 父组件
 class Board extends Component {
-    constructor () {
-        super();
-        // 9个空值数组
-        this.state = {
-            squares: Array(9).fill(null),
-            // 将X默认设置为先手
-            xIsNext: true,
-        };
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick (i) {
-        // 用.slice方法将之前的数组深拷贝到一个新的数组中，而不是修改原数组，防止对已有数据的改变
-        // 对于字符串类型，浅复制是对值的复制，
-        // 对于对象来说，浅复制是对对象地址的复制，并没有开辟新的栈，也就是复制的结果是两个对象指向同一个地址，修改其中一个对象的属性，则另一个对象的属性也会改变，
-        // 而深复制则是开辟新的栈，两个对象对应两个不同的地址，修改一个对象的属性，
-        // 不会改变另一个对象的属性。深复制实现代码如下：可以从两个方法进行解决。
-        const squares = this.state.squares.slice();
-
-        // 已经落子或有一方获胜
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        // 每走一步都需要切xIsNext的值来实现轮流
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            // 更改状态
-            xIsNext: !this.state.xIsNext
-        })
-    }
 
     // 从父组件Board传递一个事件处理函数到子组件Square中
     renderSquare(i) {
         return (
             <Square 
-                value={this.state.squares[i]} 
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]} 
+                onClick={() => this.props.onClick(i)}
             />
         )
     };
 
     render () {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner:' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -99,15 +59,84 @@ class Board extends Component {
 }
 
 class Game extends Component {
+    constructor () {
+        super();
+        this.state = {
+            history: [{
+                // 9个空值数组
+                squares: Array(9).fill(null)
+            }],
+            // 将X默认设置为先手
+            xIsNext: true,
+            stepNumber: 0
+        };
+    };
+    jumpTo (step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step%2) ? false : true
+        })
+    }
+    handleClick (i) {
+        // 用.slice方法将之前的数组深拷贝到一个新的数组中，而不是修改原数组，防止对已有数据的改变
+        // 对于字符串类型，浅复制是对值的复制，
+        // 对于对象来说，浅复制是对对象地址的复制，并没有开辟新的栈，也就是复制的结果是两个对象指向同一个地址，修改其中一个对象的属性，则另一个对象的属性也会改变，
+        // 而深复制则是开辟新的栈，两个对象对应两个不同的地址，修改一个对象的属性，
+        // 不会改变另一个对象的属性。深复制实现代码如下：可以从两个方法进行解决。
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        // 已经落子或有一方获胜
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        // 每走一步都需要切xIsNext的值来实现轮流
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        console.log(squares)
+        
+        this.setState({
+            history: history.concat([{
+                squares: squares
+            }]),
+            // 更改状态
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
+        })
+    }
+
     render () {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Move #' + move : 'Game Start';
+            return (
+                <li key={move}>
+                    <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+                </li>
+            )
+        })
+
+
+        let status;
+        if (winner) {
+            status = 'Winner:' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board 
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)} 
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* todo */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
             
@@ -131,6 +160,7 @@ function calculateWinner (squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            console.log(squares[a])
             return squares[a];
         }
     }
